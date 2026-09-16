9e0d57d627721fe2d46ac32f61f1e6048d4e9c33
Ario <Ariobashiri@gmail.com>
2026-09-16 22:47:51 +0300

    arithmetic operations

---
+++ 02 arithmetic operators/index.js
@@ -0,0 +1,19 @@
+let students = 30;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// let extraStudents %= 3;
+
+// students++;
+// students--;
+
+
+
+students -= 3;
+students += 10;
+
+document.getElementById("p1").textContent = `Number of students: ${students}`;
+document.getElementById("p2").textContent = `1 + 2 * 3 + 4 ** 2 = ${1 + 2 * 3 + 4 ** 2}`;
